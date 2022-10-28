5917258aca4c31fe4441668fcb7f112853e0d484
ijaz35 <ijaz.hossain1@gmail.com>
2022-10-28 15:30:26 +0600

    module-46

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
 import logo from './logo.svg';
 import './App.css';
+const singers = [{ name: 'Dr. Mahfuz Khan', age: 42 },
+{ name: 'Eva Rahman', age: 22 },
+{ name: 'Agun', age: 45 },
+{ name: 'Shuvro de', age: 22 }
+
+]
 
 function App() {
+  const heroes = ['Sakib Khan', 'Bappa Raj', 'Kuber', 'Josim', 'Rajjak', 'Anwar', 'Elias Kanchan'];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Players</h2>
+      {
+        heroes.map(hero => <Person name={hero}></Person>)
+      }
+      <h2>Singers</h2>
+      {
+        singers.map(singer => <Singer name={singer.name} age={singer.age}></Singer>)
+      }
+      {/* <Person name={heros[0]} nayika="Opu bishwas"></Person>
+      <Person name={heros[1]} nayika="Moushumi"></Person>
+      <Person name={heros[2]} nayika="Kopila"></Person> */}
+      <h2>My new friends</h2>
+      <Friend name="Shuvo Sheikh" phone="01722445566"></Friend>
+      <Friend name="Imran Khan" phone="01711223344"></Friend>
+      <Friend name="Imtiaz Hasan" phone="01722334455"></Friend>
+    </div>
+  );
+}
+
+function Person(props) {
+  // console.log(props);
+  return (
+    <div className="person">
+      <h1>{props.name}</h1>
+      <h3>{props.nayika}</h3>
+    </div>
+  );
+}
+function Friend(props) {
+  return (
+    <div className="friend">
+      <h2>Friend Name: {props.name}</h2>
+      <p>Phone Number: {props.phone}</p>
+    </div>
+  );
+}
+function Singer(props) {
+  console.log(props);
+  return (
+    <div className="singer">
+      <h1>{props.name}</h1>
+      <h4>{props.age}</h4>
     </div>
   );
 }
